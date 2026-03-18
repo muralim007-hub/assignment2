9c2f254120d0b1bb675640ce195529d1e65b78b4
muralim007-hub <murali.m007@gmail.com>
2026-03-19 01:12:53 +0530

    Add files via upload

---
+++ Even_Odd.py
@@ -0,0 +1,8 @@
+#ASSIGNMENT 2:Module 3: Control Structures in Python
+#Task 1: Check if a Number is Even or Odd
+num=int(input("enter number:"))
+if num%2==0:
+    print(num,"is an even number.")
+else:
+    print(num,"is odd number.")
+
